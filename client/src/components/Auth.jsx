3eac59d363d07a3d5b830481cc0b8f91c614f6da
bxljoy <259500313@qq.com>
2024-03-17 03:05:48 +0100

    tailwind styling

--- client/src/components/Auth.jsx
+++ client/src/components/Auth.jsx
@@ -1,28 +1,21 @@
 import { useState } from "react";
 import { useCookies } from "react-cookie";
-import { SIGNIN, SIGNUP, USERS, LIGHTGRAY, WHITE } from "../constants";
+import { USERS, SIGNIN, SIGNUP } from "../constants";
 
 const Auth = () => {
   const [cookies, setCookie, removeCookie] = useCookies(null);
+  const [isSignIn, setIsSignIn] = useState(true);
   const [error, setError] = useState(null);
   const [email, setEmail] = useState(null);
   const [password, setPassword] = useState(null);
-  const [confirmPassword, setConfirmPassword] = useState(null);
-  const [isLogin, setIsLogin] = useState(true);
   const serverURL = import.meta.env.VITE_SERVERURL;
 
-  const viewLogin = (status) => {
-    setError(null);
-    setIsLogin(status);
-  };
-
-  const handleSubmit = async (e, endpoint) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    if (!isLogin && password !== confirmPassword) {
-      setError("passwords do not match");
-      return;
-    }
-    const response = await fetch(`${serverURL}/${USERS}/${endpoint}`, {
+    console.log("email", email);
+    console.log("password", password);
+    const endPoint = isSignIn ? SIGNIN : SIGNUP;
+    const response = await fetch(`${serverURL}/${USERS}/${endPoint}`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -30,59 +23,115 @@ const Auth = () => {
       body: JSON.stringify({ email, password }),
     });
     const data = await response.json();
+    console.log(data);
     if (data.details) {
       setError(data.details);
     } else {
       setCookie("email", data.email);
       setCookie("authToken", data.token);
-
       window.location.reload();
     }
   };
 
   return (
-    <div className="auth-container">
-      <div className="auth-container-box">
-        <form action="">
-          <h2>{isLogin ? "please log in" : "please sign up"}</h2>
-          <input
-            type="email"
-            placeholder="email"
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <input
-            type="password"
-            placeholder="password"
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          {!isLogin && (
-            <input
-              type="password"
-              placeholder="confirm password"
-              onChange={(e) => setConfirmPassword(e.target.value)}
-            />
+    <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+        <img
+          className="mx-auto h-10 w-auto"
+          src="logo-todo.svg"
+          alt="Your Logo"
+        />
+        <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+          {isSignIn ? "Sign in to your account" : "Sign up your account"}
+        </h2>
+      </div>
+
+      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+        <form className="space-y-6" onSubmit={handleSubmit}>
+          <div>
+            <label
+              htmlFor="email"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              Email address
+            </label>
+            <div className="mt-2">
+              <input
+                id="email"
+                name="email"
+                type="email"
+                autoComplete="email"
+                required
+                onChange={(e) => setEmail(e.target.value)}
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+
+          <div>
+            <div className="flex items-center justify-between">
+              <label
+                htmlFor="password"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Password
+              </label>
+            </div>
+            <div className="mt-2">
+              <input
+                id="password"
+                name="password"
+                type="password"
+                autoComplete="current-password"
+                required
+                onChange={(e) => setPassword(e.target.value)}
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+          {!isSignIn && (
+            <div>
+              <div className="flex items-center justify-between mt-6">
+                <label
+                  htmlFor="confirmpassword"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Confirm Password
+                </label>
+              </div>
+              <div className="mt-2">
+                <input
+                  id="confirmpassword"
+                  name="confirmpassword"
+                  type="password"
+                  autoComplete="current-password"
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
           )}
-          <input
-            type="submit"
-            className="create"
-            onClick={(e) => handleSubmit(e, isLogin ? SIGNIN : SIGNUP)}
-          />
-          {error && <p className="error">{error}</p>}
+
+          <div>
+            {error && <p className="text-red-400 font-bold">{error}</p>}
+            <button
+              type="submit"
+              className="flex w-full mt-2 justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              {isSignIn ? "Sign in" : "Sign up"}
+            </button>
+          </div>
         </form>
-        <div className="auth-options">
+
+        <p className="mt-10 text-end text-sm text-gray-500">
+          {isSignIn ? "Don't have an account?" : "Already have an account?"}{" "}
           <button
-            onClick={() => viewLogin(false)}
-            style={{ backgroundColor: !isLogin ? LIGHTGRAY : WHITE }}
+            className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+            onClick={() => setIsSignIn(!isSignIn)}
           >
-            sign up
+            {isSignIn ? "Sign Up" : "Sign In"}
           </button>
-          <button
-            onClick={() => viewLogin(true)}
-            style={{ backgroundColor: isLogin ? LIGHTGRAY : WHITE }}
-          >
-            login
-          </button>
-        </div>
+        </p>
       </div>
     </div>
   );
--- client/src/components/ListHeader.jsx
+++ client/src/components/ListHeader.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import Modal from "./Modal";
 import { useCookies } from "react-cookie";
 
-const ListHeader = ({ listName, fetchData }) => {
+const ListHeader = ({ listName, userEmail, fetchData }) => {
   const [cookies, setCookie, removeCookie] = useCookies(null);
   const [showModal, setShowModal] = useState(false);
   const signOut = () => {
@@ -13,14 +13,29 @@ const ListHeader = ({ listName, fetchData }) => {
   };
 
   return (
-    <div className="list-header">
-      <h1>{listName}</h1>
-      <div className="button-container">
-        <button className="create" onClick={() => setShowModal(true)}>
+    <div className="w-full">
+      <div className="flex flex-row max-md:flex-col justify-between max-md:justify-center items-center gap-4 py-8 mb-8 rounded-lg bg-purple-500">
+        <h1 className="text-4xl max-lg:text-2xl font-bold lg:ml-4 text-white">
+          {listName}
+        </h1>
+        <div className="flex flex-row max-md:flex-col justify-center items-center lg:gap-10 gap-4">
+          <p className="text-white">
+            Hello, <span className="text-white font-bold">{userEmail}</span>
+          </p>
+          <button
+            className="text-white font-bold lg:mr-8 hover:text-orange-500"
+            onClick={signOut}
+          >
+            Sign Out
+          </button>
+        </div>
+      </div>
+      <div className="text-end max-md:text-center my-6">
+        <button
+          className="text-bold bg-blue-500 rounded-lg text-white px-4 py-2 leading-6 font-semibold hover:bg-blue-800"
+          onClick={() => setShowModal(true)}
+        >
           Add Task
-        </button>
-        <button className="signout" onClick={signOut}>
-          Sign Out
         </button>
       </div>
       {showModal && (
